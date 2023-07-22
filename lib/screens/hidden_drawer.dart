538e6ef0139d68b31c502be8418ae8570dc4fef3
rahul2409 <2016.rahul.nandrajog@ves.ac.in>
2023-07-23 00:42:58 +0530

    Font awesome added and drawer redefining started.

--- lib/screens/hidden_drawer.dart
+++ lib/screens/hidden_drawer.dart
@@ -14,8 +14,20 @@ class HiddenDrawer extends StatelessWidget {
     "Membership Registrations": "add_membership_registrations"
   };
   @override
-  Widget build(BuildContext context) {
-    if (session["privilege"] == "StoreOwner") {
+  Widget build(BuildContext context) => SingleChildScrollView(
+        child: Column(
+          children: [
+            buildDrawerItems(context),
+          ],
+        ),
+      );
+  Widget buildDrawerItems(BuildContext context) => Container();
+}
+
+
+/**
+ * 
+ * if (session["privilege"] == "StoreOwner") {
       drawerNames.clear();
       drawerNames = {
         "Add Membership": "add_membership",
@@ -84,76 +96,4 @@ class HiddenDrawer extends StatelessWidget {
       ),
     );
   }
-}
-
-
-// ListView(padding: EdgeInsets.zero, children: [
-//             ListTile(
-//               title: Text(
-//                 'User Email',
-//                 style: customTextStyle(),
-//               ),
-//             ),
-//             SizedBox(
-//               height: 60,
-//             ),
-//             drawerListTile(
-//               'Home',
-//               context,
-//               '/add_store',
-//               const Icon(Icons.home),
-//             ),
-//             Container(
-//               width: 70,
-//               height: 1,
-//               color: Colors.grey[300],
-//             ),
-//             ListTile(
-//               leading: Icon(Icons.leaderboard_rounded),
-//               title: Text(
-//                 "Leaderboard",
-//                 style: customTextStyle(),
-//               ),
-//             ),
-//             Container(
-//               width: 70,
-//               height: 1,
-//               color: Colors.grey[300],
-//             ),
-//             ListTile(
-//               leading: Icon(Icons.comment),
-//               title: Text(
-//                 "About",
-//                 style: customTextStyle(),
-//               ),
-//             ),
-//             Container(
-//               width: 70,
-//               height: 1,
-//               color: Colors.grey[300],
-//             ),
-//             ListTile(
-//               leading: Icon(Icons.phone),
-//               title: Text(
-//                 "Contact",
-//                 style: customTextStyle(),
-//               ),
-//             ),
-//             Container(
-//               width: 70,
-//               height: 1,
-//               color: Colors.grey[300],
-//             ),
-//             SizedBox(
-//               height: 100,
-//             ),
-//             ListTile(
-//                 leading: CircleAvatar(
-//               radius: 30,
-//               backgroundColor: Colors.blue[900]!,
-//               child: Icon(
-//                 Icons.logout,
-//                 color: Colors.white,
-//               ),
-//             )),
-//           ]),
+ */
